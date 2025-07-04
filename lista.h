a8763534581d638a0527171e18884d0912ebbfca
renan-jose <renan.jose2024@uel.br>
2025-07-04 16:30:09 -0300

    Adição da lista.h
    lista.h contém o <manual> para implementar uma lista.

---
+++ lista.h
@@ -0,0 +1,46 @@
+/* Explicar a lista. */
+
+/* Proteção contra múltiplas inclusões, garantindo que este cabeçalho seja incluído apenas uma vez 
+ * durante a compilação, evitando redefinições e conflitos. */
+#ifndef _LISTA_H_
+#define _LISTA_H_
+
+/* Importação das bibliotecas */
+#include <stdbool.h>
+
+typedef void* Lista; /* Lista representa uma lista ao todo. */
+typedef void* DadoGenerico; /* DadoGenerico representa um dado genérico a ser armazenado na lista. */
+
+/* Inicialização */
+
+/* Cria e retorna uma nova lista genérica vazia. */
+Lista inicializarLista();
+
+
+/* Inserções */
+
+/* Insere um dado genérico (qualquer tipo) no início da lista. */
+void inserirInicioLista(Lista, DadoGenerico);
+
+
+/* Remoções */
+
+/* Remove o primeiro elemento da lista. */
+void removerInicioLista(Lista);
+
+
+/* Buscas/Verificações */
+
+/* Retorna o número total de elementos presentes na lista. */
+int buscarTamanhoLista(Lista);
+
+/* Retorna verdadeiro se o dado estiver na lista. */
+bool verificarElementoLista(Lista, DadoGenerico);
+
+
+/* Desalocação */
+
+/* Libera toda a memória associada à lista. */
+void desalocarLista(Lista);
+
+#endif /* _LISTA_H_ */
