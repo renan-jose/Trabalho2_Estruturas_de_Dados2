58a15db0c7208c21907f6f4f5630bbcfb3fcda74
renan-jose <renan.jose2024@uel.br>
2025-07-04 16:43:41 -0300

    Adição da main.c
    main.c é a <cabeça> de todo o projeto, e o guia também.

---
+++ main.c
@@ -0,0 +1,14 @@
+/* Importação das bibliotecas */
+//#include "graph.h"
+#include "espalhamento.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/*******************************************************************************/
+
+int main(/*Colocar os parametros*/){
+
+    
+
+    return 0;
+}
