9f1b14a84855a5b495a39396ab285b2c4ad8547d
renan-jose <renan.jose2024@uel.br>
2025-07-23 09:38:27 -0300

    Aprimoramento de processamento.h
    Os parâmetros da função processarArquivos foram aprimorados.

---
+++ renansantos/src/processamento.h
@@ -0,0 +1,12 @@
+/* Proteção contra múltiplas inclusões, garantindo que este cabeçalho seja incluído apenas uma vez 
+ * durante a compilação, evitando redefinições e conflitos. */
+#ifndef _PROCESSAMENTO_H_
+#define _PROCESSAMENTO_H_
+
+// Nomes alternativos para os tipos de dados existentes.
+typedef void* GrafoGenerico;
+
+// Explicar depois  
+void processarArquivos(char *, char *,  char *, char *, char *);
+
+#endif // _PROCESSAMENTO_H_
